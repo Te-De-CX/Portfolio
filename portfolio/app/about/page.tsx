aa24fd0a4dd513e62122d41daea41d5e8e870183
Teslim <tunjiheritage@gmail.com>
2025-09-04 21:06:28 +0100

    done

--- portfolio/app/about/page.tsx
+++ portfolio/app/about/page.tsx
@@ -1,6 +1,6 @@
 
 import AboutHome from "@/components/components/about/home";
-import SnakeGame from "@/components/components/about/SnakeGame";
+// import SnakeGame from "@/components/components/about/SnakeGame";
 import FAQs from "@/components/components/about/Faq";
 import Footer from "@/components/UI/layouts/footer";
 // import Certifications from "@/components/UI/contents/Certificatins";
@@ -11,7 +11,7 @@ const AboutPage = () => {
         <>
             <AboutHome />
             {/* <Certifications /> */}
-            <SnakeGame />
+            {/* <SnakeGame /> */}
             <FAQs />
             <Footer />
         </>
